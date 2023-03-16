f63f6e96f04ed51bc49fe5246bd18478f0074c72
khanh.tran <xuankhanhyb@gmail.com>
2023-03-16 10:12:28 +0700

    Add state when redirect to authorize

--- src/main/java/com/formlaez/auth/web/AuthController.java
+++ src/main/java/com/formlaez/auth/web/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
 import java.util.Objects;
 
 @Controller
@@ -18,12 +19,16 @@ public class AuthController {
 	private final AppOAuth2ClientProperties properties;
 
 	@GetMapping("auth")
-	public String prepareLogin(@RequestParam(name = "client_id", required = false) String clientId) {
+	public String prepareLogin(@RequestParam(name = "client_id", required = false) String clientId,
+							   @RequestParam(name = "state", required = false) String state) {
 		clientId = Objects.requireNonNullElse(clientId, properties.getDefaultClient());
 		var client = clientRepo.findByClientId(clientId);
 		Assert.notNull(client, "Client not found");
 		var redirectUris = client.getRedirectUris();
-		return "redirect:/oauth2/authorize?response_type=code&client_id=" + client.getClientId() + "&redirect_uri=" + redirectUris.iterator().next();
+		return "redirect:/oauth2/authorize?response_type=code&client_id="
+				+ client.getClientId()
+				+ "&redirect_uri=" + redirectUris.iterator().next()
+				+ "&state=" + state;
 	}
 
 }
